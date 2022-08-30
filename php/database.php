91a1692f0e4919e47ddbeb9918de89cf8c36d8df
Rex Bianchi <rexbianchi@protonmail.com>
2022-08-30 10:09:36 -0400

    API refactor

--- php/database.php
+++ php/database.php
@@ -12,7 +12,7 @@ class Database
             # TODO => change variable placeholders
             $this->connection = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE_NAME);
          
-            if ( mysqli_connect_errno()) {
+            if (mysqli_connect_errno()) {
                 throw new Exception("Could not connect to database.");   
             }
         } catch (Exception $e) {
