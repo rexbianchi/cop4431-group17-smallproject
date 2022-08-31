229c21afbbe0b6c56ea4580177acba652de2d205
Rex Bianchi <rexbianchi@protonmail.com>
2022-08-31 19:04:37 -0400

    typo in add user

--- php/api/add_contact.php
+++ php/api/add_contact.php
@@ -18,22 +18,8 @@ if($connection->connect_error){
 }
 
 
-$statement = $mysqli->prepare("INSERT INTO User (first_name, last_name, email, password) USING (?, ?, ?, ?);");
-$statement->bind_param("ssss", $in_data["first_name"], $in_data["last_name"], $in_data["username"], $in_data["password"]);
-
-$statement->execute();
-
-if($statement->error){
-    send_JSON_error();
-}
-
-// Only need to return that it was successful
-send_JSON_response("");
-
-// Close DB connections
-$statement->close();
-$connection->close();
-
+$statement = $mysqli->prepare("INSERT INTO Contact (user_id, first_name, last_name, email, phone_number) USING (?, ?, ?, ?);");
+$statement->bind_param("issss", $in_data["first_name"], $in_data["last_name"], $in_data["username"], $in_data["password"]);
 
 // If statement is successful, then return JSON response
 if($statement->execute()) {
