64d482bb0f791a12504021155b6c297a8e9a79bb
npro1001 <nprocacciojr@gmail.com>
2022-09-07 11:36:57 -0400

    % back to local host because changed mysql config

--- php/config.php
+++ php/config.php
@@ -1,6 +1,6 @@
 <?php
     return array(
-        'db_host' => '%',
+        'db_host' => 'localhost',
         'db_username' => 'Users',
         'db_password' => 'Group17',
         'db_name' => 'COP4331'
