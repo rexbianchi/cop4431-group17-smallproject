32b072512010ebb363c0203129f3d5a5cc3d3bcc
npro1001 <nprocacciojr@gmail.com>
2022-09-07 11:31:59 -0400

    wildcard % not localhost

--- php/config.php
+++ php/config.php
@@ -1,7 +1,7 @@
 <?php
     return array(
-        'db_host' => 'localhost',
-        'db_username' => 'User',
+        'db_host' => '%',
+        'db_username' => 'Users',
         'db_password' => 'Group17',
         'db_name' => 'COP4331'
         )
