a79c6b1b467ef93457877aa1d2c341ef9738b2b1
npro1001 <nprocacciojr@gmail.com>
2022-09-07 11:48:13 -0400

    attempting to use the root to debut mysql connection errors

--- php/config.php
+++ php/config.php
@@ -1,8 +1,8 @@
 <?php
     return array(
         'db_host' => 'localhost',
-        'db_username' => 'User',
-        'db_password' => 'Group17',
+        'db_username' => 'root',
+        'db_password' => 'a.4RavKakBY93vz',
         'db_name' => 'COP4331'
         )
 ?>
