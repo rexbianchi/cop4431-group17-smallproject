a99d4f8da40396dbbf0e8f2b3cb3866a4e0d90aa
Rex Bianchi <rexbianchi@protonmail.com>
2022-09-08 18:34:44 -0400

    fixing response

--- php/api/get_contacts.php
+++ php/api/get_contacts.php
@@ -32,7 +32,7 @@ $searchCount = 0;
 $searchResults = "";
 $searchTerm = "%".$in_data["search"]."%";
 
-$statement = $connection->prepare("SELECT FirstName,LastName FROM Contacts WHERE FirstName like ? AND UserId = ? OR LastName like ? AND UserId = ?");
+$statement = $connection->prepare("SELECT FirstName, LastName, Email, PhoneNumber FROM Contacts WHERE FirstName like ? AND UserId = ? OR LastName like ? AND UserId = ?");
 $statement->bind_param("ssss", $searchTerm , $in_data["id"], $searchTerm, $in_data["id"]);
 $statement->execute();
 $result = $statement->get_result();
@@ -46,9 +46,9 @@ $id = $in_data["id"];
 //     $searchCount++;
 //     $searchResults .= $row["FirstName"] . ' ' . $row["LastName"];
 // }
-
+ $searchResults = array();
 while($row = $result->fetch_assoc()) {
-    $searchResults .= $row["FirstName"] . ' ' . $row["LastName"];
+    array_push($searchResults, $row);
 }
 
 if(is_null($id)) {
