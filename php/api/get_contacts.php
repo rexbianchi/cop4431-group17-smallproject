2d4ae025aeff224687067c83531afc151c592d5d
npro1001 <nprocacciojr@gmail.com>
2022-09-10 19:23:32 -0400

    bug fixes

--- php/api/get_contacts.php
+++ php/api/get_contacts.php
@@ -12,7 +12,7 @@ $configs = include("../config.php");
 $in_data = get_request_info();
 $id = $in_data["id"];
 $page = $in_data['page'];
-$search_term = "%".$in_data["search"]."%";
+$search_term = $in_data["search"];
 $row_offset = 0; // 0, 10, 20, etc...
 $default_amt = 3;
 
@@ -41,27 +41,27 @@ if($connection->connect_error)
 }
 
 // If "search" parameter is null -> return default_amt of rows @ page x
-// if(is_null($search_term)) {
-//     $statement = $connection->prepare("SELECT FirstName, LastName, Email, PhoneNumber, Id FROM Contacts ORDER BY Id LIMIT 3,3");
-//     // $statement->bind_param("ii", $row_offset, $default_amt);
-//     $statement->execute();
-//     $result = $statement->get_result();
+if(is_null($search_term) || empty($search_term)) { 
+    $statement = $connection->prepare("SELECT FirstName, LastName, Email, PhoneNumber, Id FROM Contacts ORDER BY Id LIMIT ?,?");
+    $statement->bind_param("ii", $row_offset, $default_amt);
+    $statement->execute();
+    $result = $statement->get_result();
 
-//     $search_results = array();
-//     while($row = $result->fetch_assoc()) {
-//         array_push($search_results, $row);
-//     }
+    $search_results = array();
+    while($row = $result->fetch_assoc()) {
+        array_push($search_results, $row);
+    }
 
-//     if($row = $result->fetch_assoc()) {
-//         send_JSON_error($statement->error);
-//     }
-//     else {
-//         send_JSON_response($search_results); 
-//     }
+    if($row = $result->fetch_assoc()) {
+        send_JSON_error($statement->error);
+    }
+    else {
+        send_JSON_response($search_results); 
+    }
 
-// }
-// // If "search" parameter is not null -> return search results
-// else {
+}
+// If "search" parameter is not null -> return search results
+else {
     $statement = $connection->prepare("SELECT FirstName, LastName, Email, PhoneNumber, Id FROM Contacts WHERE FirstName like ? AND UserId = ? OR LastName like ? AND UserId = ?");
     $statement->bind_param("ssss", $search_term , $in_data["id"], $search_term, $in_data["id"]);
     $statement->execute();
@@ -84,7 +84,7 @@ if($connection->connect_error)
         send_JSON_response($search_results); 
     }
 
-// }
+}
 
 // TODO: Limit amount of rows returned by search query (use pagination)
 
