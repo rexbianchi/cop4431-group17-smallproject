d0243c987b0b1420cc8cab570e9deb5ce937ff83
Joshua Easterling <joshua.easterling3@icloud.com>
2022-09-10 18:51:13 -0400

    Progress on login js

--- js/frontend.js
+++ js/frontend.js
@@ -1,4 +1,4 @@
-const urlBase = 'http://cop4331-group17-sp.info/php/api/';
+const urlBase = 'http://cop4331-group17-sp.info/php/api';
 const extension = 'php';
 
 let userId = 0;
@@ -13,7 +13,7 @@ function doLogin()
 	
 	let login = document.getElementById("loginName").value;
 	let password = document.getElementById("loginPassword").value;
-	var hash = md5( password );
+	//var hash = md5( password );
 	
 	document.getElementById("loginResult").innerHTML = "";
 
@@ -72,6 +72,46 @@ function doLogout()
 	window.location.href = "index.html";
 }
 
+function saveCookie()
+{
+	let minutes = 20;
+	let date = new Date();
+	date.setTime(date.getTime()+(minutes*60*1000));	
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+}
+
+function readCookie()
+{
+	userId = -1;
+	let data = document.cookie;
+	let splits = data.split(",");
+	for(var i = 0; i < splits.length; i++) 
+	{
+		let thisOne = splits[i].trim();
+		let tokens = thisOne.split("=");
+		if( tokens[0] == "firstName" )
+		{
+			firstName = tokens[1];
+		}
+		else if( tokens[0] == "lastName" )
+		{
+			lastName = tokens[1];
+		}
+		else if( tokens[0] == "userId" )
+		{
+			userId = parseInt( tokens[1].trim() );
+		}
+	}
+	
+	if( userId < 0 )
+	{
+		window.location.href = "index.html";
+	}
+	else
+	{
+		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+	}
+}
 function addUser()
 {
 	
