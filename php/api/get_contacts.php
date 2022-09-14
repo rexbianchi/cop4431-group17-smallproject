cd05051aa53f686b56933c837aa2691894b1ead4
npro1001 <nprocacciojr@gmail.com>
2022-09-13 20:28:53 -0400

    fidx

--- php/api/get_contacts.php
+++ php/api/get_contacts.php
@@ -12,9 +12,9 @@ $configs = include("../config.php");
 $in_data = get_request_info(); // get_query_params();
 $id = $in_data['id'];
 $page = $in_data['page'];
-if(in_array('search', $in_data)) {
-    $search_term = "%".$in_data['search']."%";
-}
+
+$search_term = "%".$in_data['search']."%"; // if(in_array('search', $in_data)) {
+    
 $row_offset = 0; // 0, 10, 20, etc...
 $default_amt = 3;
 
