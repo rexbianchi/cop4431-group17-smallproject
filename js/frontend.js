1b0e2eeaecf92bb2ca726bfe7b2b53d2bba2ac2b
Jesse Johnson <jessejohnson1423@gmail.com>
2022-09-19 17:41:53 -0400

    Hiding edit and trash button test 2

--- js/frontend.js
+++ js/frontend.js
@@ -7,7 +7,7 @@ let lastName = "";
 let pageNum = 1;
 let numOfContacts = 0;
 let editOrDeleteID = -1;
-let editMode = 0;
+let editFlag = 0;
 
 
 // alert box
@@ -501,7 +501,7 @@ function deleteContact() {
 }
 
 function revealContactButtons(ID) {
-	if(editMode == 1) return;
+	if(editFlag == 1) return;
 
 	let instance = document.getElementById(ID);
 
