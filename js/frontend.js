4c30c382a22b1ed4adf7c5ea741a80c2a7e2b13b
Joshua Easterling <joshua.easterling3@icloud.com>
2022-09-20 20:04:10 -0400

    moved add button again

--- js/frontend.js
+++ js/frontend.js
@@ -11,16 +11,16 @@ let editFlag = 0;
 
 
 // alert box
-if (document.getElementById) {
-	// Swap the native alert for the custom
-	// alert
-	window.alert = function (alert_message) {
-		custom_alert(alert_message);
+	if (document.getElementById) {
+		// Swap the native alert for the custom
+		// alert
+		window.alert = function (alert_message) {
+			custom_alert(alert_message);
+		}
 	}
-}
 
 
-document.getElementById('prevPage').style.display = "none";
+
 
 function doLogin() {
 	userId = -1;
@@ -43,12 +43,16 @@ function doLogin() {
 	let xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try {
-		xhr.onreadystatechange = function () {
-			if (this.readyState == 4 && this.status == 200) {
-				let jsonObject = JSON.parse(xhr.responseText);
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				let jsonObject = JSON.parse( xhr.responseText );
 
-				if (jsonObject.status == 'failure') {
+				if(jsonObject.status == 'failure')
+				{		
 					document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
 					return;
 				}
@@ -72,24 +76,24 @@ function doLogin() {
 }
 
 // Where the java script should be
-function eyeButton() {
+function eyeButton(){
 	const togglePassword = document.querySelector('#toggle-password');
-	const password = document.querySelector('#password');
+    const password = document.querySelector('#password');
 
-	togglePassword.addEventListener('click', clickEyeButton);
+    togglePassword.addEventListener('click', clickEyeButton);
 }
-function clickEyeButton() {
-	// toggle the type attribute
-	const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-	password.setAttribute('type', type);
-	// toggle the eye slash icon
-	this.classList.toggle('fa-eye-slash');
+function clickEyeButton(){
+     // toggle the type attribute
+	 const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+	 password.setAttribute('type', type);
+	 // toggle the eye slash icon
+	 this.classList.toggle('fa-eye-slash');
 }
 
 function createAccount() {
 
 	userId = 0;
-
+	
 
 	let username = document.getElementById("username").value;
 	let password = document.getElementById("password").value;
@@ -97,15 +101,23 @@ function createAccount() {
 	let lastname = document.getElementById("lastname").value;
 	document.getElementById("createResult").innerHTML = "";
 
-	if (username == "")
+	if(username == '')
+	{
 		return;
-	if (password == "")
+	}
+	if(password == '')
+	{
 		return;
-	if (firstname == "")
+	}
+	if(firstname == '')
+	{
 		return;
-	if (lastname == "")
+	}
+	if(lastname == '')
+	{
 		return;
-
+	}
+		
 
 
 	let tmp = { first_name: firstname, last_name: lastname, username: username, password: password }
@@ -120,7 +132,6 @@ function createAccount() {
 			if (this.readyState == 4 && this.status == 200) {
 
 				let jsonObject = JSON.parse(xhr.responseText);
-				
 				userId = jsonObject.response.Id;
 
 
@@ -142,7 +153,11 @@ function createAccount() {
 	catch (err) {
 		document.getElementById("loginResult").innerHTML = err.message;
 	}
-}
+
+
+
+	
+
 
 function doLogout() {
 	userId = 0;
@@ -186,14 +201,14 @@ function readCookie() {
 
 function addUser() {
 	editFlag = 0;
-
+	
 	let email = document.getElementById("addEmail").value;
 	let firstname = document.getElementById("addFirstName").value;
 	let lastname = document.getElementById("addLastName").value;
 	let phoneNumber = document.getElementById("addPhoneNumber").value;
 	document.getElementById("addResult").innerHTML = "";
 
-	let tmp = { first_name: firstname, last_name: lastname, email: email, phone_number: phoneNumber, user_id: userId }
+	let tmp = { first_name: firstname, last_name: lastname, email: email, phone_number:phoneNumber, user_id: userId }
 	let jsonPayload = JSON.stringify(tmp);
 	let url = urlBase + '/add_contact.' + extension;
 
@@ -361,7 +376,6 @@ function getContacts() {
 			}
 		};
 		xhr.send(jsonPayload);
-		
 	}
 	catch (err) {
 		document.querySelector("#data-output").innerHTML = err.message;
@@ -378,74 +392,14 @@ function getContacts() {
 }
 
 function incrementPageNum() {
-	var prevPageDisplay = document.getElementById('prevPage');
-	var nextPageDisplay = document.getElementById('nextPage');
-	if(prevPageDisplay.style.display === "none"){
-		prevPageDisplay.style.display = "block";
-	}
-		
-	let url = urlBase + '/get_contacts.' + extension;
-
-	let srch = document.getElementById("search").value;
-	document.getElementById("pageNum").innerHTML = pageNum;
-
-	let tmp = { id: userId, page: pageNum, search: srch };
-	let jsonPayload = JSON.stringify(tmp);
-
-	let xhr = new XMLHttpRequest();
-	// open(method, url, async)
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try {
-		xhr.onreadystatechange = function () {
-			if (this.readyState == 4 && this.status == 200) {
-				let placeholder = document.querySelector("#data-output");
-				let jsonObject = JSON.parse(xhr.responseText);
-
-				let out = "";
-				let contactID;
-
-				let result = jsonObject.response;
-				if(result.length){
-					pageNum++;
-					getContacts();
-				}else{
-					nextPageDisplay.style.display = "none";	
-				}
-
-			}
-		};
-		xhr.send(jsonPayload);
-		
-	}
-	catch (err) {
-		document.querySelector("#data-output").innerHTML = err.message;
-	}
-		
-
+	pageNum++;
+	getContacts();
 }
-
 
 function decrementPageNum() {
-	var nextPageDisplay = document.getElementById('nextPage');
-	var prevPageDisplay = document.getElementById('prevPage')
-	if(nextPageDisplay.style.display === "none"){
-		nextPageDisplay.style.display = "block";
-	}
-		
-	if(pageNum - 1 === 1){
-
-		prevPageDisplay.style.display = "none";
-		pageNum--;
-		getContacts();
-	}
-	else{
-		pageNum--;
-		getContacts();
-	}
-	
+	pageNum--;
+	getContacts();
 }
-
 
 function editMode(ID) {
 	editFlag = 1; // turn = 1 later?
@@ -462,7 +416,7 @@ function editMode(ID) {
 	//let buttons = instance.getElementsByTagName("div");
 
 	//instance.removeChild(buttons);
-
+ 
 	let rowID = editOrDeleteID + "Element";
 
 	let rowContent = document.getElementById(rowID);
@@ -472,7 +426,7 @@ function editMode(ID) {
 	let lastname = rowContent.children[1].innerHTML;
 	let phoneNumber = rowContent.children[3].children[0].innerHTML;
 
-	let newInput = `
+	let newInput= `
 		<td><input class="form-control" type="text" id="newFirstName" value="${firstname}"></td>
 		<td><input class="form-control" type="textEmail" id="newLastName" value="${lastname}"></td>
 		<td><input class="form-control" type="text" id="newEmail" value="${email}"></td>
@@ -501,7 +455,7 @@ function cancelEdit() {
 }
 
 function saveEdit() {
-
+	
 	//$statement->bind_param("ssssi", $in_data["first_name"], $in_data["last_name"], $in_data["email"], $in_data["phone_number"], $in_data["Id"]);
 
 	let url = urlBase + '/edit_contact.' + extension;
@@ -511,7 +465,7 @@ function saveEdit() {
 	let lastname = document.getElementById("newLastName").value;
 	let phoneNumber = document.getElementById("newPhoneNumber").value;
 
-	let tmp = { first_name: firstname, last_name: lastname, email: email, phone_number: phoneNumber, Id: editOrDeleteID };
+	let tmp = { first_name: firstname, last_name:lastname, email: email, phone_number: phoneNumber, Id: editOrDeleteID };
 	let jsonPayload = JSON.stringify(tmp);
 
 	let xhr = new XMLHttpRequest();
@@ -567,7 +521,7 @@ function deleteContact() {
 }
 
 function revealContactButtons(ID) {
-	if (editFlag == 1) return;
+	if(editFlag == 1) return;
 
 	let instance = document.getElementById(ID);
 
